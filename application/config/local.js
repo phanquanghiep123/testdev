613bb6274d956ae3ec8313b28458c819559bee4b
DESKTOP-AJAK4UI\phanq <phanquanghiep123@gmail.com>
2017-12-15 17:00:57 +0700

    f

--- application/config/local.js
+++ application/config/local.js
@@ -1,5 +1,8 @@
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+
+console.log(process.env);
 _Config.local = {
 	host : ip,
 	port : port,
