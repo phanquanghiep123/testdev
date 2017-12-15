3b55b506348574e6cf48d278810fea7c0586cb54
DESKTOP-AJAK4UI\phanq <phanquanghiep123@gmail.com>
2017-12-15 16:10:14 +0700

    h

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || 'fristabc-testdev.1d35.starter-us-east-1.openshiftapps.com';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var express = require('express');
 var app = express();
 app.get('/', function(req, res){
