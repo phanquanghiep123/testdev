346a9ef83ca7e72bafee422618b800146453e427
DESKTOP-AJAK4UI\phanq <phanquanghiep123@gmail.com>
2017-12-15 17:29:02 +0700

    f

--- application/controllers/home.js
+++ application/controllers/home.js
@@ -1,7 +1,7 @@
 function home (){
 	this.extent   = MyController;
 	this.index = function(){
-		var  env = console.log(process.env); 
+		var  env = process.env; 
 		for(var i in env){
 			write ("dfgdfgdg <br>");
 			write(i + ":" + env[i]);
